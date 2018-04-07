5b7bc6249c9829b2ddc31e5a1f2630b6a429c60e
Yuriy Brusentsov <yuriybrusentsov@gmail.com>
2018-04-08 00:37:57 +0300

    init

---
+++ Notes/MainViewModel.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Notes
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs args) {
+            if (PropertyChanged != null)
+                PropertyChanged(this, args);
+        }
+    }
+
+    public class DelegateCommand : ICommand
+    {
+        public delegate void ICommandOnExecute(object parameter);
+        public delegate bool ICommandOnCanExecute(object parameter);
+
+        private ICommandOnExecute _execute;
+        private ICommandOnCanExecute _canExecute;
+
+        public DelegateCommand(ICommandOnExecute onExecuteMethod, ICommandOnCanExecute onCanExecuteMethod)
+        {
+            _execute = onExecuteMethod;
+            _canExecute = onCanExecuteMethod;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute.Invoke(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute.Invoke(parameter);
+        }
+    }
+
+    class MainViewModel : ViewModelBase
+    {
+        private DelegateCommand exitCommand;
+
+        public NoteModel NotesArr { get; set; }
+
+        public string TextToCreate { get; set; }
+        public string TopicToCreate { get; set; }
+
+        public string TextToEdit { get; set; }
+
+        public MainViewModel()
+        {
+            this.NotesArr = NoteModel.Current;
+        }
+
+        public ICommand ExitCommand
+        {
+            get
+            {
+                if (null == exitCommand)
+                {
+                    exitCommand = new DelegateCommand(Exit, CanExecuteCommandTrue);
+                }
+
+                return exitCommand;
+            }
+        }
+
+        private void Exit(object parameter)
+        {
+            // Save data before an exit.
+            MessageBoxResult result = MessageBox.Show("Do you want to save changes?", "Save confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                NotesArr.SaveNote();
+            }
+
+            Application.Current.Shutdown();
+        }
+
+        public bool CanExecuteCommandTrue(object parameter)
+        {
+            return true;
+        }
+
+        private ICommand createNote;
+        public ICommand CreateNote
+        {
+            get
+            {
+                Console.WriteLine("CreateNote ICommand");
+                if (null == createNote)
+                {
+                    createNote = new DelegateCommand(CheckAndInvokeCommand, CanExecuteCommandTrue);
+
+                }
+                return createNote;
+            }
+        }
+
+        public void CheckAndInvokeCommand(object parameter)
+        {
+            Console.WriteLine("CheckAndInvokeCommand");
+            if (TextToCreate == null)
+                throw new ArgumentException("Text is null");
+            if (TopicToCreate == null)
+                throw new ArgumentException("Topic is null");
+
+            NotesArr.CreateNote(TopicToCreate, TextToCreate, DateTime.Now);
+        }
+
+        public void DeleteNote(object NoteObj)
+        {
+            NotesArr.Remove((Note)NoteObj);
+        }
+
+        public void EditNote(object NoteObj)
+        {
+            Note oldNote = (Note)NoteObj;
+            int index = NotesArr.GetNoteIndex(oldNote);
+            NotesArr.RemoveNote(oldNote);
+
+            Note newNote = new Note();
+            newNote.Text = TextToEdit;
+            NotesArr.CreateNote(newNote, index);
+        }
+    }
+}
---
+++ Notes/NoteModel.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes
+{
+    [Serializable]
+    public class Note
+    {
+        public string text;
+        public string topic;
+        public DateTime time;
+        public string timeToShow;
+
+        public string Text
+        {
+            get
+            {
+                return this.text;
+            }
+            set
+            {
+                if (!String.IsNullOrEmpty(value))
+                {
+                    this.text = value;
+                }
+            }
+        }
+
+        public string Topic
+        {
+            get
+            {
+                return this.topic;
+            }
+            set
+            {
+                if (!String.IsNullOrEmpty(value))
+                {
+                    this.topic = value;
+                }
+            }
+        }
+
+        public DateTime Time
+        {
+            get
+            {
+                return this.time;
+            }
+            set
+            {
+                if (typeof(DateTime) == value.GetType())
+                {
+                    this.time = value;
+                }
+            }
+        }
+
+        public string TimeToShow
+        {
+            get
+            {
+                return this.GetFormattedNoteTime();
+            }
+            set
+            {
+                if (!String.IsNullOrEmpty(value))
+                {
+                    this.timeToShow = value;
+                }
+            }
+        }
+
+        public Note()
+        {
+            this.Text = "Hey, i'm your first note. Double click on me to change me.";
+            this.Topic = "Sample topic.";
+            this.Time = DateTime.Now;
+            this.TimeToShow = Time.ToString();
+        }
+
+        public Note(string topic, string text, DateTime time)
+            : this()
+        {
+            this.Topic = topic;
+            this.Text = text;
+            this.Time = time;
+            this.TimeToShow = time.ToString();
+        }
+
+        private string GetFormattedNoteTime()
+        {
+            return this.time.ToShortTimeString();
+        }
+    }
+
+
+    public class NoteModel : ObservableCollection<Note>
+    {
+        private static object _threadLock = new Object();
+        private static NoteModel current = null;
+
+        public static NoteModel Current
+        {
+            get
+            {
+                Console.WriteLine("current");
+                lock (_threadLock)
+                    if (null == current)
+                        current = new NoteModel();
+                return current;
+            }
+        }
+
+        private NoteModel()
+        {
+            Note[] NoteArr = DeserializeNotes();
+            foreach (Note NoteObj in NoteArr)
+            {
+                Add(NoteObj);
+            }
+            Console.WriteLine(this.Items);
+        }
+
+        public int GetNoteIndex(Note Note)
+        {
+            return IndexOf(Note);
+        }
+
+        public void RemoveNote(Note Note)
+        {
+            Remove(Note);
+        }
+
+        public void CreateNote(Note Note, int index)
+        {
+            Insert(index, Note);
+        }
+
+        public void CreateNote(string topic, string text, DateTime time)
+        {
+            Note NoteObj = new Note(topic, text, time);
+            Add(NoteObj);
+            Console.WriteLine("number of Notes: " + this.Count);
+        }
+
+        public void SaveNote()
+        {
+            SerializeNotes(Current.ToArray());
+        }
+
+        // Serialization.
+        public static void SerializeNotes(Note[] NoteArr)
+        {
+            FileStream fs = new FileStream("Notes.dat", FileMode.Create);
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(fs, NoteArr);
+            fs.Close();
+        }
+
+        // Deserialization.
+        public static Note[] DeserializeNotes()
+        {
+            Note[] NoteBuf;
+            try
+            {
+                FileStream fs = new FileStream("Notes.dat", FileMode.Open);
+                BinaryFormatter bf = new BinaryFormatter();
+                NoteBuf = (Note[])bf.Deserialize(fs);
+
+                fs.Close();
+            }
+            catch (Exception e)
+            {
+                NoteBuf = new Note[] { new Note(), };
+            }
+
+            return NoteBuf;
+        }
+    }
+}
