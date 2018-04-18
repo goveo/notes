02d4c77f6673323dd87990d5d317786c234e7f06
Yuriy Brusentsov <yuriybrusentsov@gmail.com>
2018-04-19 01:03:28 +0300

    smth close to proxy

--- Notes/Views/NoteEditor.xaml.cs
+++ Notes/Views/NoteEditor.xaml.cs
@@ -19,19 +19,82 @@ namespace Notes.Views
         public event Action<string> NewText;
         public event Action<string> NewTopic;
 
+        ButtonChecker buttonChecker;
+        public SmartEditButton Button { get; set; }
+
         public void editNoteClicked(object sender, RoutedEventArgs e)
         {
             System.Diagnostics.Debug.WriteLine("editNoteClicked");
-            if (NewText != null && NewTopic != null)
-            {
-                NewTopic(TopicField.Text);
-                NewText(TextField.Text);
-            }
+
+            //if (NewText != null && NewTopic != null)
+            //{
+            NewTopic(TopicField.Text);
+            NewText(TextField.Text);
+            //}
             this.Close();
         }
         public NoteEditor()
         {
             InitializeComponent();
+
+            Button.realButton = EditButton;
+            Button.realButton.IsEnabled = false;
+
+            buttonChecker = new ButtonChecker(TopicField.Text, TextField.Text);
         }
+
+        public class SmartEditButton
+        {
+            public Button realButton;
+            public bool isEnabled;
+        }
+        //interface
+        interface IButtonEnabler
+        {
+            void EnableButton(SmartEditButton button);
+        }
+
+        //realsubject
+        class ButtonEnabler : IButtonEnabler
+        {
+            public void EnableButton(SmartEditButton button)
+            {
+                button.isEnabled = true;
+            }
+        }
+
+        //proxy
+        class ButtonChecker : IButtonEnabler
+        {
+            string topic;
+            string description;
+            public ButtonChecker(string topic, string description)
+            {
+                this.topic = topic;
+                this.description = description;
+            }
+            ButtonEnabler btnEnabler = new ButtonEnabler();
+            public void EnableButton(SmartEditButton smartButton)
+            {
+                if (topic != null && description != null)
+                {
+                    btnEnabler.EnableButton(smartButton);
+                } 
+                else
+                {
+                    smartButton.realButton.IsEnabled = false;
+                }
+            }
+        }
+
+        private void TextChanged(object sender, RoutedEventArgs e)
+        {
+            buttonChecker.EnableButton(Button);
+        }
+        private void TitleChanged(object sender, RoutedEventArgs e)
+        {
+            buttonChecker.EnableButton(Button);
+        }
+
     }
 }
